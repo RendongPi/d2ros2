1eaa9b1bfc1833a784d975b6a62c3d5fd3e452ea
rdpi <rendong.pi@outlook.com>
2022-08-17 09:03:29 +0000

    0817

---
+++ chapt3/chapt3_ws/build/example_interfaces_rclpy/colcon_command_prefix_setup_py.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/rdpi/Documents/TieDan/d2ros2/chapt3/chapt3_ws/install/example_ros2_interfaces/share/example_ros2_interfaces/package.sh"
