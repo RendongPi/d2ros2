8ba6ff0a55ca825ab24f2d1ce32f249cafb43283
rdpi <rendong.pi@outlook.com>
2022-08-17 14:02:58 +0000

    launch file

--- launch_test/launch_ws/src/robot_startup/launch/example_action.launch.py
+++ launch_test/launch_ws/src/robot_startup/launch/example_action.launch.py
@@ -6,11 +6,11 @@ def generate_launch_description():
     """launch内容描述函数，由ros2 launch 扫描调用"""
     action_robot_01 = Node(
         package="example_action_rclpy",
-        executable="action_robot_02.py"
+        executable="action_robot_02"
     )
     action_control_01 = Node(
         package="example_action_rclpy",
-        executable="action_control_02.py"
+        executable="action_control_02"
     )
     # 创建LaunchDescription对象launch_description,用于描述launch文件
     launch_description = LaunchDescription(
