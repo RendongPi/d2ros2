2edd3e94a224f72cfaaf06752416562de4a1503a
rdpi <rendong.pi@outlook.com>
2022-08-17 09:13:09 +0000

    delete build etc

--- chapt3/chapt3_ws/build/example_interfaces_rclpy/colcon_command_prefix_setup_py.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/rdpi/Documents/TieDan/d2ros2/chapt3/chapt3_ws/install/example_ros2_interfaces/share/example_ros2_interfaces/package.sh"
